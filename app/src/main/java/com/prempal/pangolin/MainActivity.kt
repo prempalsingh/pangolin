cb689d95109e61b887f10a237d7dc1bcf8b6b907
Prempal Singh <prempal.42@gmail.com>
2023-04-17 15:51:08 +0530

    add ktlint

--- app/src/main/java/com/prempal/pangolin/MainActivity.kt
+++ app/src/main/java/com/prempal/pangolin/MainActivity.kt
@@ -18,7 +18,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             PangolinTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
